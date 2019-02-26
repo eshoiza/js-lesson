a83b93f1d79d696aa051a8c5ce38dee0cd619d1c
jamesoyanna <engrjayt200@gmail.com>
2019-02-26 15:31:24 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,118 @@
+var name  =  "James";
+var myNumber = 12;
+var myBoolean = true;
+
+
+console.log("Name:" + name);
+console.log("Winning Number is:" + myNumber);
+console.log("Buhari will win:" + myBoolean);
+
+// strings
+//Numbers
+//Boolean   true or false
+
+/*
+document.write("welcome home");
+
+function sayHello(){
+    alert("Hello my people");
+}
+
+
+
+const list = new Set();
+const adding = list.add(1).add(2).add(4);
+console.log(adding);
+
+const number  = new Set([1,3,5,4]);
+console.log(number);
+
+const words = new Set().add("the").add("is coiming").add("tomorow");
+console.log(words.has("the"));
+console.log(words.delete("the"));
+console.log(words);
+
+const shoppingSet = new Set().add("beans").add("garri").add('fish');
+const shopArray = [...shoppingSet]
+console.log(shopArray);
+
+const numerals = new Map();
+console.log(numerals.set(1,"i").set(3,"iii").set(2,"ii"));
+const getam = numerals.get(3);
+console.log(getam);
+console.log(numerals.has(3));
+console.log(numerals.size);
+const clearNow = numerals.clear();
+console.log(clearNow);
+*/
+
+            // IF STATEMENTS
+const age  = 28;
+if(age<18){
+    document.write("You are too young to vote");
+}
+
+const n  =13;
+if(n%2===0){
+    console.log("n is an even number");
+} else{
+    console.log("n is an odd number");
+}
+
+
+
+var jane = new Object();
+jane.lastname = 'samuel'
+jane.job = 'Hair Dresser';
+jane['school'] = 'convenant';
+jane['surnname'] = 'gomina';
+console.log(jane);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
